6d00ec20025251624b9e1e62a8e0e099b57366d6
BS-GavinC <98208007+VZGLY@users.noreply.github.com>
2023-06-19 15:44:03 +0200

    UpdatExo4

--- script.js
+++ script.js
@@ -26,33 +26,47 @@ let ajouter = (item) => {
     refreshPanier();
 }
 
+let retirer = (item) => {
+    if (!panier[item]) {
+        return;
+    }
+    else if(panier[item] == 1){
+        delete panier[item]
+    }
+    else{
+        panier[item]--
+    }
+
+    refreshPanier()
+}
+
+
 let refreshPanier = () => {
     let tabPanier = document.getElementById("Panier")
     tabPanier.innerHTML = ""
     let prixTotal = 0
     for (const prop in panier) {
-
-        for (let index = 0; index < panier[prop]; index++) {
-            tabPanier.appendChild(createPanierRow(prop, articles[prop]))
-            prixTotal += articles[prop];
-        }
-
+        tabPanier.appendChild(createPanierRow(prop, articles[prop], panier[prop]))
+        prixTotal += articles[prop] * panier[prop];
     }
 
-    tabPanier.appendChild(createPanierRow("Total", prixTotal))
+    tabPanier.appendChild(createPanierRow("Total", prixTotal, ""))
     
 }
 
-let createPanierRow = (nomLabel, prixLabel) => {
+let createPanierRow = (nomLabel, prixLabel, nbrLabel) => {
     let tr = document.createElement("tr");
     let nom = document.createElement("td")
     let prix = document.createElement("td")
+    let nbr = document.createElement("td")
 
     nom.innerHTML = nomLabel;
-    prix.innerHTML = prixLabel
+    prix.innerHTML = prixLabel;
+    nbr.innerHTML = nbrLabel;
 
     tr.appendChild(nom)
     tr.appendChild(prix)
+    tr.appendChild(nbr)
 
     return tr;
 }
@@ -67,6 +81,8 @@ let createArticleRow = (nomLabel, prixLabel) => {
     tr.appendChild(nom);
     tr.appendChild(prix);
     tr.innerHTML += `<button onclick="ajouter('${nomLabel}')"> Ajouter </button>`
+    tr.innerHTML += `<button onclick="retirer('${nomLabel}')"> Retirer </button>`
+    
     return tr;
 }
 
